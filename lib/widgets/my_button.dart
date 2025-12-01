d4ead48d3595cf2a396dbc2fa033f900bb914727
sanstha1 <sthasantosh070@gmail.com>
2025-12-01 22:20:15 +0545

    Widgets added

---
+++ lib/widgets/my_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton({
+    super.key,
+    required this.onPressed,
+    required this.text,
+  });
+
+  final VoidCallback onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 30,
+            vertical: 10,
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
