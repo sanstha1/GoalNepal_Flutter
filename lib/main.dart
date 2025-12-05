37fe0dad0e50c157c6d56f7c8a5e3f25ad5a6f9b
sanstha1 <sthasantosh070@gmail.com>
2025-12-05 10:56:18 +0545

    Added splash screen and animations too

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:goal_nepal/screens/onboarding_screen.dart';
+// import 'package:goal_nepal/screens/onboarding_screen.dart';
+import 'package:goal_nepal/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:OnboardingScreen()
+      home:SplashScreen()
     );
   }
 }
