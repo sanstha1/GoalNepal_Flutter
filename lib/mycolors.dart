f057e3f55902ecf5a5a73b1428e4d9cea62963aa
sanstha1 <sthasantosh070@gmail.com>
2025-12-05 16:20:05 +0545

    images added

---
+++ lib/mycolors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color lightYellow = Color(0xFFFFFFE3);
+  static const Color lightGray = Color(0xFFCBCBCB);
+  static const Color darkGray = Color(0xFF4A4A4A);
+  static const Color blueGray = Color(0xFF6D8196);
+}
