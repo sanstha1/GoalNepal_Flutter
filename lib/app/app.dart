f2e8c27bce3892f2be803b9b9fba2a9e159de6a1
sanstha1 <sthasantosh070@gmail.com>
2026-01-01 21:58:09 +0545

    Clean architecture applied

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:goal_nepal/screens/splash_screen.dart';
+// import 'package:goal_nepal/main.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Goal Nepal',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      home: const SplashScreen(),
+    );
+  }
+}
